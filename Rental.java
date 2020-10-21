30713f505921719b786afa8c9516fbab9f31bf6f
isadoracristina <isadorac2008@hotmail.com>
2020-10-20 21:41:47 -0300

    Commit 10

--- Rental.java
+++ Rental.java
@@ -20,10 +20,7 @@ public class Rental {
     return _movie.getCharge(_daysRented);
   }
 
-  public int getFrequentRenterPoints() { 
-    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-         return 2;
-      else 
-         return 1;
+  public int getFrequentRenterPoints(){
+    return _movie.getFrequentRenterPoints(_daysRented);
   }
 }
